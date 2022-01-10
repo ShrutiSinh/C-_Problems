555f0660c205eaede9b156cefe606e9cf43bd1f3
ShrutiSinh <93397931+ShrutiSinh@users.noreply.github.com>
2022-01-10 18:03:10 +0530

    Add files via upload

---
+++ Basic_Problems/Problem2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter the no.s\n";
+    cin>>x>>y;
+    cout<<"sum of "<<x<<" and "<<y<<" is "<<x+y;
+    return 0;
+
+}
