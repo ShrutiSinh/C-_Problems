c04bbf427d970b6cce2a5e30a56352b186cccdb9
ShrutiSinh <93397931+ShrutiSinh@users.noreply.github.com>
2022-01-11 18:36:03 +0530

    Add files via upload

---
+++ Basic_Problems/problem4.cpp
@@ -0,0 +1,15 @@
+/*
+ Write a program in C++ to print the sum of two numbers using variables.
+Print the sum of two numbers :
+-----------------------------------
+The sum of 29 and 30 is : 59
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<"The sum of "<<x<<" and "<<y<<" is :"<<x+y;
+    return 0;
+
+}
